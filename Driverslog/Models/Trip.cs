e43cb43ad79de3c161ed32f7c925a6d918fe7c49
christoferlof <christofer.lof@gmail.com>
2010-05-23 22:33:02 +0200

    first look att isolated storage serialization

---
+++ Driverslog/Models/Trip.cs
@@ -0,0 +1,22 @@
+using System.Runtime.Serialization;
+
+namespace Driverslog.Models {
+    [DataContract]
+    public class Trip {
+        
+        [DataMember]
+        public string From { get; set; }
+
+        [DataMember]
+        public string To { get; set; }
+
+        [DataMember]
+        public int OdometerStart { get; set; }
+
+        [DataMember]
+        public int OdometerStop { get; set; }
+
+        [DataMember]
+        public string Notes { get; set; }
+    }
+}
--- Driverslog/ViewModels/CreateTripViewModel.cs
+++ Driverslog/ViewModels/CreateTripViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Runtime.Serialization.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -8,17 +11,15 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
 using Driverslog.Commands;
+using Driverslog.Models;
 
 namespace Driverslog.ViewModels {
     public class CreateTripViewModel {
 
         public CreateTripViewModel() {
             SaveTripCommand = new DelegateCommand(SaveTrip);
-        }
-
-        private void SaveTrip(object obj) {
-            MessageBox.Show("foo");
         }
 
         public string From { get; set; }
@@ -34,5 +35,28 @@ namespace Driverslog.ViewModels {
         public string Notes { get; set; }
 
         public ICommand SaveTripCommand { get; private set; }
+
+        private void SaveTrip(object obj) {
+
+            //serialize trip
+            var path = "trips.json";
+            var storage = IsolatedStorageFile.GetUserStoreForApplication();
+            
+            var file = storage.OpenFile(path, FileMode.OpenOrCreate);
+            file.Seek(0, SeekOrigin.Begin);
+            
+            var serializer = new DataContractJsonSerializer(typeof (TripList));
+            var list = serializer.ReadObject(file) as TripList;
+            if(list == null) {
+                list = new TripList();
+            }
+
+            list.Trips.Add(new Trip());
+
+            file.Seek(0,SeekOrigin.Begin);
+            serializer.WriteObject(file, list);
+
+            file.Close();
+        }
     }
 }

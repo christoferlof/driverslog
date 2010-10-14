86b15323e84593ca174c77c8e0665773412b3506
Christofer Lof <christofer.lof@gmail.com>
2010-10-14 22:05:33 +0200

    refactor build script

--- default.ps1
+++ default.ps1
@@ -1,21 +1,23 @@
 properties {
-  $basedir = resolve-path .
-  $builddir = "$basedir\build"
-  $toolsdir = "$basedir\tools"
+  $basedir    = resolve-path .
+  $builddir   = "$basedir\build"
+  $toolsdir   = "$basedir\tools"
   $packagedir = "$toolsdir\Package"
-  $xapdir = "$toolsdir\Content\ClientBin"
-  $slnfile = "$basedir\Driverslog.sln"
+  $xapdir     = "$toolsdir\Content\ClientBin"
+  $slnfile    = "$basedir\Driverslog.sln"
   
 }
 
 task default -depends Test
 
 task Test -depends Compile, Clean { 
-  copy-item $builddir\Driverslog*.dll $packagedir
-  copy-item $builddir\Victoria*.dll $packagedir
-  copy-item $builddir\System.Xml.Linq.dll $packagedir
-  copy-item $builddir\System.Windows.Interactivity.dll $packagedir
-  copy-item $builddir\Caliburn.Micro.dll $packagedir
+  @(
+    'Driverslog*.dll',
+    'Victoria*.dll',
+    'System.Xml.Linq.dll',
+    'System.Windows.Interactivity.dll',
+    'Caliburn.Micro.dll'
+  ) | foreach { copy-item $builddir\$_ $packagedir }
   
   $env:path = "$toolsdir;$env:path"
   exec {Victoria.Test.Console.exe $testpath} "Testrun failed"
@@ -26,9 +28,9 @@ task Compile -depends Clean {
 }
 
 task Clean { 
-  get-childitem $builddir -recurse | remove-item -recurse
-  get-childitem $packagedir -recurse | remove-item -recurse
-  get-childitem $xapdir -recurse | remove-item -recurse
+  @($builddir, $packagedir, $xapdir) | foreach {
+    get-childitem $_ -recurse | remove-item -recurse
+  }
 }
 
 task ? -Description "Helper to display task info" {
--- test.ps1
+++ test.ps1
@@ -1,8 +1,3 @@
-#$basedir = resolve-path .
-#msbuild "/p:OutDir=$basedir\build\"  /nologo /verbosity:quiet
-#if($?){
-#  Fx\Victoria.Test.Console\bin\debug\Victoria.Test.Console.exe $args[0]
-#}
 $testpath = ""
 if($args[0] -ne $null) {
   $testpath = $args[0]

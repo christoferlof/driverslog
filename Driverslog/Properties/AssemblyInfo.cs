f862bdadcb247d7ef5108bb0649cb56b344e71a0
christoferlof <christofer.lof@gmail.com>
2011-02-09 22:30:39 +0100

    release 1.0.0.0

--- Driverslog/Properties/AssemblyInfo.cs
+++ Driverslog/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Driver's log")]
-[assembly: AssemblyCopyright("Copyright © Christofer Löf 2010")]
+[assembly: AssemblyCopyright("Copyright © Christofer Löf 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

2c6585c088be3396e5a15ba104db16985252f8a5
christoferlof <christofer.lof@gmail.com>
2011-03-02 13:00:54 +0100

    add pivot to mainview

--- Driverslog/MainPage.xaml.cs
+++ Driverslog/MainPage.xaml.cs
@@ -7,12 +7,14 @@ using System.Net;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Resources;
 using System.Windows.Shapes;
+using Driverslog.Models;
 using Driverslog.ViewModels;
 using Microsoft.Phone.Controls;
 
@@ -20,6 +22,23 @@ namespace Driverslog {
     public partial class MainPage : PhoneApplicationPage {
         public MainPage() {
             InitializeComponent();
+        }
+
+        private void TodayViewSourceFilter(object sender, FilterEventArgs e) {
+            if (e.Item is Expense) {
+                e.Accepted = ((Expense)e.Item).Date.Date == DateTime.Today.Date;
+            }
+            if (e.Item is Trip) {
+                e.Accepted = ((Trip)e.Item).Date.Date == DateTime.Today.Date;
+            }
+        }
+
+        private void TripViewSourceFilter(object sender, FilterEventArgs e) {
+            e.Accepted = e.Item is Trip;
+        }
+
+        private void ExpenseViewSourceFilter(object sender, FilterEventArgs e) {
+            e.Accepted = e.Item is Expense;
         }
     }
 }

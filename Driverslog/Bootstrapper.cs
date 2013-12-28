33d3e7a0588d52a7b6417577224cc22391aa90a5
christoferlof <christofer.lof@live.se>
2013-12-28 21:05:25 +0100

    enable fix mainpage navigation with fast app resume

--- Driverslog/Bootstrapper.cs
+++ Driverslog/Bootstrapper.cs
@@ -7,9 +7,17 @@ using Microsoft.ApplicationInsights.Telemetry.WindowsStore;
 using Microsoft.Phone.Tasks;
 
 namespace Driverslog {
+    using System.Windows.Navigation;
+
+    using Microsoft.Phone.Controls;
+
     public class Bootstrapper : PhoneBootstrapper {
 
         private PhoneContainer _container;
+
+        private PhoneApplicationFrame _rootFrame;
+
+        private bool _reset;
 
         protected override void Configure() {
             _container = new PhoneContainer();
@@ -29,6 +37,23 @@ namespace Driverslog {
             _container.RegisterInstance(typeof(IAnalyticsService),null, new AnalyticsService());
             _container.RegisterInstance(typeof(ITrialService), null, new TrialService(new AnalyticsService()));
 
+            //intercept navigation for app resume
+            _rootFrame.Navigated += RootFrameOnNavigated;
+            _rootFrame.Navigating += RootFrameOnNavigating;
+
+        }
+
+        private void RootFrameOnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (_reset && e.IsCancelable && e.Uri.OriginalString == "/MainPage.xaml") {
+                e.Cancel = true;
+                _reset = false;
+            }
+        }
+
+        private void RootFrameOnNavigated(object sender, NavigationEventArgs e)
+        {
+            _reset = e.NavigationMode == NavigationMode.Reset;
         }
 
         protected override object GetInstance(Type service, string key) {
@@ -46,6 +71,12 @@ namespace Driverslog {
         protected override void OnUnhandledException(object sender, System.Windows.ApplicationUnhandledExceptionEventArgs e) {
             ClientAnalyticsChannel.Default.LogEvent("exception", new Dictionary<string, object> { {"message", e.ExceptionObject.Message}});
             base.OnUnhandledException(sender, e);
+        }
+
+        protected override PhoneApplicationFrame CreatePhoneApplicationFrame()
+        {
+            _rootFrame = new PhoneApplicationFrame();
+            return _rootFrame;
         }
     }
 }
